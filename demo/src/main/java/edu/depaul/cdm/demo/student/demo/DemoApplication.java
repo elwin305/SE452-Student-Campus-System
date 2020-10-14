1976849c8e82741b8dc7be4eb74df29417ea2069
artakhademi <71783178+artakhademi@users.noreply.github.com>
2020-10-14 00:29:16 -0500

    Add files via upload

--- demo/src/main/java/edu/depaul/cdm/demo/student/demo/DemoApplication.java
+++ demo/src/main/java/edu/depaul/cdm/demo/student/demo/DemoApplication.java
@@ -3,13 +3,14 @@ package edu.depaul.cdm.demo.student.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-        //Here it should call for the UI to start the program with the login panel
-        //The user can login as STUDENT, TEACHER and ADMIN
+        ConnectJDBC con = new ConnectJDBC();
+        con.getCoursesData();
     }
 }
