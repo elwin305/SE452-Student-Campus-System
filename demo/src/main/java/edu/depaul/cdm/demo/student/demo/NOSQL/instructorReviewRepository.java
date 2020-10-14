9373d032967f73cc7b11d8c5ff870451c45c44b7
artakhademi <71783178+artakhademi@users.noreply.github.com>
2020-10-14 12:48:40 -0500

    old file
    old file

--- demo/src/main/java/edu/depaul/cdm/demo/student/demo/NOSQL/instructorReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package group6.project.student_campus_system;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface instructorReviewRepository extends MongoRepository<instructorReview, Integer> {
-
-}
