1b8d630df33daf7ae7a3db779318b290d8d26e4f
elwin305 <71735505+elwin305@users.noreply.github.com>
2020-10-14 11:32:53 -0500

    Delete insturctorReviewRepository.java

--- demo/src/main/java/edu/depaul/cdm/demo/student/demo/NOSQL/insturctorReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package mongo6.example.group6mongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface insturctorReviewRepository extends MongoRepository<insturctorReview, Integer> {
-
-}
