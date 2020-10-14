71d1661053fb03cc2ec05ca6c4d8b4a4c5d6bd30
elwin305 <71735505+elwin305@users.noreply.github.com>
2020-10-14 11:31:11 -0500

    Add files via upload

---
+++ demo/src/main/java/edu/depaul/cdm/demo/student/demo/NOSQL/insturctorReviewRepository.java
@@ -0,0 +1,7 @@
+package mongo6.example.group6mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface insturctorReviewRepository extends MongoRepository<insturctorReview, Integer> {
+
+}
